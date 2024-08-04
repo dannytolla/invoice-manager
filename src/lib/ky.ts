c48e28c2d46d39e40acf748faf6d0ad7c6ece74c
Danny <danieltda99@gmail.com>
2024-08-04 09:47:20 +0300

    chore: Improve code readability by removing unused imports and comments

--- src/lib/ky.ts
+++ src/lib/ky.ts
@@ -6,6 +6,6 @@ const kyInstance = ky.create({
       if (key.endsWith("At")) return new Date(value);
       return value;
     }),
-});
+}); // If a key ends with "At", it means that the value associated with that key represents a date. else, return the value
 
 export default kyInstance;
